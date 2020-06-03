e29cc3bf8d9d0bcb1bd1132d33745b3ef51d8b2e
Hillol372812 <hillol.cse@gmail.com>
2020-06-03 09:04:17 +0530

    hanges done by DemoX

--- src/test/java/JiraApi_Demo/Gittest.java
+++ src/test/java/JiraApi_Demo/Gittest.java
@@ -5,6 +5,8 @@ public class Gittest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git Test");
+		System.out.println("Git Test by demo X");
+		
 
 	}
 
